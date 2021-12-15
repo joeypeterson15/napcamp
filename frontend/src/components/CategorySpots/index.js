e6a9127fc12183ed13d68732d1643fee58808553
Joey Peterson <joey.peterson15@gmail.com>
2021-12-15 15:35:24 -0800

    styled saves plus location

--- frontend/src/components/CategorySpots/index.js
+++ frontend/src/components/CategorySpots/index.js
@@ -2,19 +2,19 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import GetLocation from '../GetLocation';
 
 import GoogleMap from '../GoogleMap';
 import './CategorySpots.css'
 
-function CategorySpots ({category, spots, currentSpot, spot}) {
+function CategorySpots ({category, spots, currentSpot, propSpot}) {
 
     const dispatch = useDispatch();
 
 
 
     const [categorySpots, setCategorySpots] = useState({})
-    // const [location, setLocation] = useState('')
-    const [currentLocation, setCurrentLocation] = useState('')
+
 
     useEffect(() => {
 
@@ -31,68 +31,9 @@ function CategorySpots ({category, spots, currentSpot, spot}) {
 
 
 
-    const getLocation = (spot) => {
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${spot?.location},+CA&key=AIzaSyDcQHD-tKZVqVpv07vx0eE9lhueTMnbkyI`)
-        .then(res => res.json())
-        .then(results => results?.results[0])
-        .then((location) => {
-            console.log('Distance between input location', location)
-            var R = 6378137; // Earth’s mean radius in meter
-            var dLat = rad(currentLocation?.lat - location?.geometry?.location?.lat);
-            var dLong = rad(currentLocation?.lng - location?.geometry?.location?.lng);
-            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-              Math.cos(rad(location?.geometry?.location?.lat)) * Math.cos(rad(currentLocation?.lat)) *
-              Math.sin(dLong / 2) * Math.sin(dLong / 2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            var d = R * c;
-            console.log('d for distance', d)
-            return d; // returns the distance in meter
-        })
-        .catch(e => console.log(e));
-        }
 
 
 
-        useEffect(() => {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                  (position) => {
-                    const pos = {
-                      lat: position.coords.latitude,
-                      lng: position.coords.longitude,
-                    };
-
-                    setCurrentLocation(pos)
-                    console.log('this is current location', pos)
-                  },
-                );
-              }
-        }, [dispatch])
-
-
-
-
-
-        console.log('get distance function', getLocation(spot))
-
-
-
-    const rad = function(x) {
-        return x * Math.PI / 180;
-      };
-
-    const distanceBetween = (location) => {
-        console.log('Distance between input location', location)
-        var R = 6378137; // Earth’s mean radius in meter
-        var dLat = rad(currentLocation?.lat - location?.geometry?.location?.lat);
-        var dLong = rad(currentLocation?.lng - location?.geometry?.location?.lng);
-        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.cos(rad(location?.geometry?.location?.lat)) * Math.cos(rad(currentLocation?.lat)) *
-          Math.sin(dLong / 2) * Math.sin(dLong / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c;
-        return d; // returns the distance in meter
-      };
 
 
     return (
@@ -100,13 +41,16 @@ function CategorySpots ({category, spots, currentSpot, spot}) {
             {Object.values(categorySpots).length > 0 ? <h5 className="text">{`More ${category} listings`}</h5> : <div></div>}
 
                 {Object.values(categorySpots).map((spot) => (
-                    <div className="category-card">
-                        <Link className="category-link text" to={`/spots/${spot.id}`}>
+                    <Link className="category-link text" to={`/spots/${spot.id}`}>
+                            <div className="category-card">
                                 <img className="category-image" src={spot.imageUrl} alt={spot.id}></img>
-                                <div className="category-name" key={spot.name}>{spot.name}</div>
-                                <div>{getLocation(spot)}</div>
-                        </Link>
+                                <div>
+                                    <div className="category-name" key={spot.name}>{spot.name}</div>
+                                    <GetLocation spot={spot}/>
+
+                                </div>
                     </div>
+                        </Link>
                 ))}
 
 
