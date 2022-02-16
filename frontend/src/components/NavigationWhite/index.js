8ae942425797df9dcb73203651581cd57aba6100
Joey Peterson <joey.peterson15@gmail.com>
2022-02-16 10:53:59 -0800

    search and stuff

--- frontend/src/components/NavigationWhite/index.js
+++ frontend/src/components/NavigationWhite/index.js
@@ -5,6 +5,11 @@ import ProfileButton from './ProfileButton';
 import * as sessionActions from '../../store/session'
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+// import { Dispatch } from 'react';
+import { searchForLocations } from '../../store/spots';
+
 import './NavigationWhite.css';
 
 
@@ -13,12 +18,24 @@ import './NavigationWhite.css';
 
 function NavigationWhite({ isLoaded, spotId }){
 
-  console.log('hellllo', spotId)
+  let history = useHistory()
+
+  const searchLocation = (e) => {
+    e.preventDefault();
+    setLocation('')
+    dispatch(searchForLocations(location))
+    history.push(`/search/${location.toLowerCase()}`)
+
+}
+
+  const isTrips = window.location.href.includes('trips')
 
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user);
   const [credential, setCredential] = useState('')
   const [password, setPassword] = useState('')
+  const [location, setLocation] = useState('')
+
 
   const demoLogin = async () => {
     setCredential("Demo-lition")
@@ -55,6 +72,16 @@ function NavigationWhite({ isLoaded, spotId }){
       <div>
         <NavLink id="napcamp-text" className="nav-link-login" exact to="/">NapCamp</NavLink>
       </div>
+      {isTrips ?
+      <div className="search-nav-div">
+        <input className="search-input-nav"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        type="text"
+        placeholder="Search a Location..."
+        ></input>
+        <button id="search-button-nav" onClick={(e) => searchLocation(e)}><div id="search-icon-nav-div"><i className="fas fa-search"></i></div></button>
+      </div> : ""}
       <div className="right-nav">
         <Link to="/trips" className="nav-link trips-tag">Trips</Link>
         <Link to="/saves" className="nav-link">Saves</Link>
